1ccbe3eee573d7b3042fc96ce2e14d4f39afef4b
iftekharr001 <iftekharr001@gmail.com>
2023-12-22 03:57:41 -0800

    updated server side

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: "https://technominds-9a66d.web.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
 //middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
@@ -32,9 +40,86 @@ async function run() {
     const usersCollection = client.db("technoDb").collection("users");
     const addProductsCollection = client.db("technoDb").collection("addProducts");
     const checkOutProductsCollection = client.db("technoDb").collection("checkOut");
+    const paymentProductsCollection = client.db("technoDb").collection("payments");
 
 
-   
+ app.post("/checkOut/:id", async (req, res) => {
+   try {
+     const id = req.params.id;
+     const { productId, quantity, image, name, discount, cost, email } = req.body;
+      const checkoutData = {
+        productId: new ObjectId(productId),
+        quantity: quantity,
+        image: image,
+        name: name,
+        discount: discount,
+        cost: cost,
+        email: email,
+      };
+
+     const result = await checkOutProductsCollection.insertOne(checkoutData);
+
+     res.send(result);
+   } catch (error) {
+     console.error("Error processing checkout:", error);
+     res.status(500).send({ error: "Internal Server Error" });
+   }
+ });
+ app.get("/checkOut", async(req, res) => {
+  const email = req.query.email;
+  if (email) {
+    const query = { email: email };
+    const cursor = checkOutProductsCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+  } else {
+    const allData = await shopCollection.find({}).toArray();
+    res.send(allData);
+  }
+ });
+
+  app.post("/payments", async (req, res) => {
+    try {
+      const payment = req.body;
+
+      // Make sure payment object and cardIds property are present
+      if (!payment || !payment.cardIds) {
+        return res.status(400).send({ error: "Invalid payment data" });
+      }
+
+      const paymentResult = await paymentProductsCollection.insertOne(payment);
+
+      const query = {
+        _id: {
+          $in: payment.cardIds.map((id) => new ObjectId(id)),
+        },
+      };
+
+      const deleteResult = await checkOutProductsCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
+    } catch (error) {
+      console.error("Error processing payment:", error);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  });
+
+
+
+ app.post('/create-payment-intent', async(req, res) =>{
+  const {cost} = req.body;
+  const amount = parseInt(cost * 100);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd', 
+    payment_method_types: ['card']
+
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+ })
 
     app.post('/addProducts', async(req, res) => {
        const item = req.body;
@@ -42,9 +127,7 @@ async function run() {
        res.send(result);
     });
 
- app.post("/checkOut", async (req, res) => {
-   
- });
+
 
 
     app.get('/addProducts', async(req, res) => {
@@ -59,6 +142,16 @@ async function run() {
         res.send(allData);
       }
     });
+    app.get('/addProducts', async(req, res) =>{
+      const filter = req.query;
+      const query = {
+        _id: {$regex: filter.search}
+      }
+      const cursor = addProductsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+   
 
     app.patch('/addProducts/:id', async(req, res) => {
       const item = req.body;
@@ -125,6 +218,16 @@ async function run() {
         const result = await usersCollection.insertOne(user);
         res.send(result);
     });
+     app.get("/users/admin/:email", async (req, res) => {
+       const email = req.params.email;
+       const query = { email: email };
+       const user = await usersCollection.findOne(query);
+       let admin = false;
+       if(user){
+        admin = user?.role === 'admin';
+       }
+       res.send({admin});
+     });
     app.get('/users', async(req, res) => {
       const cursor = usersCollection.find();
       const result = await cursor.toArray();
@@ -144,10 +247,10 @@ async function run() {
 
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
